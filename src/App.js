6fad29c8e690227981c7ae18796290697e01ba07
antonijek <antonijek@yahoo.com>
2021-02-25 16:29:38 +0100

    image and menu

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import "./assets/output.css";
 
 function App() {
   return (
-    <div >
-      
-     </div>
+    <div class=" bg-purple-300  h-24 p-5">
+    <nav class="flex  md:justify-between">
+        <div class="flex w-full justify-between">
+          <img src='https://www.eparhija.me/images/manastir-somina.jpg' className= ' border-2 border-black hover:border-blue-500 w-20 md:w-24'></img>
+
+           <p  class="md:hidden ">
+             menu
+           </p>
+        </div>
+       
+        <ul class="hidden md:flex" id="mobileMenu">
+            <li class="pr-5"><a> Services </a></li>
+            <li class="pr-5"><a>Porfolio</a></li>
+            <li class="pr-5"><a>About</a></li>
+            <li><a>Contact</a></li>
+        </ul>
+    </nav>
+</div>
   );
 }
 

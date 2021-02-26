e1c0b1dd5e67fc4116d591e5c636b9992dbb7c70
Nikola <zelezekas@gmail.com>
2021-02-26 13:46:16 +0100

    Antonijeva nova verzija headera

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "./assets/output.css";
-import Footer from "./Footer";
 
 function App() {
   return (
-    <div class=" relative bg-purple-300  h-24 p-5">
+    <div class=" bg-blue-200   ">
     <nav class="flex  md:justify-between">
         <div class="flex w-full justify-between">
-          <img src='https://www.eparhija.me/images/manastir-somina.jpg' className= ' border-2 border-black hover:border-blue-500 w-20 md:w-24'></img>
+          <img src='https://www.eparhija.me/images/manastir-somina.jpg' className= ' w-28 md:w-24'></img>
 
-           <p  class="md:hidden ">
-             menu
+           <p  class="md:hidden  font-bold  uppercase p-6">
+            <a href='#'> menu</a>
            </p>
         </div>
-       
-        <ul class="hidden md:flex" id="mobileMenu">
-            <li class="pr-5"><a> Services </a></li>
-            <li class="pr-5"><a>Porfolio</a></li>
-            <li class="pr-5"><a>About</a></li>
-            <li><a>Contact</a></li>
+
+        <ul class="hidden md:flex items-center  font-bold pr-4 " id="mobileMenu">
+            <li class="pr-5 hover:underline"><a href='#'> Istorijat </a></li>
+            <li class="pr-5"><a href='#'>Galerija</a></li>
+            <li class="pr-5"><a  href='#'>Manastir</a></li>
+            <li><a href='#'>Kontakt</a></li>
         </ul>
+         <button className='bg-blue-400 w-10 h-6 hover:bg-green-200  font-black'>EN</button>
     </nav>
-    <Footer/>
+
+
 </div>
   );
 }

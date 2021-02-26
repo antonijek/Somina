1006529d328a839eec00190b7dd91d8c1263bed6
antonijek <antonijek@yahoo.com>
2021-02-26 12:50:53 +0100

    button

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import "./assets/output.css";
 
 function App() {
   return (
-    <div class=" bg-purple-300  h-24 p-5">
+    <div class=" bg-blue-200   ">
     <nav class="flex  md:justify-between">
         <div class="flex w-full justify-between">
-          <img src='https://www.eparhija.me/images/manastir-somina.jpg' className= ' border-2 border-black hover:border-blue-500 w-20 md:w-24'></img>
+          <img src='https://www.eparhija.me/images/manastir-somina.jpg' className= ' w-28 md:w-24'></img>
 
-           <p  class="md:hidden ">
-             menu
+           <p  class="md:hidden  font-bold  uppercase p-6">
+            <a href='#'> menu</a>
            </p>
         </div>
        
-        <ul class="hidden md:flex" id="mobileMenu">
-            <li class="pr-5"><a> Services </a></li>
-            <li class="pr-5"><a>Porfolio</a></li>
-            <li class="pr-5"><a>About</a></li>
-            <li><a>Contact</a></li>
+        <ul class="hidden md:flex items-center  font-bold pr-4 " id="mobileMenu">
+            <li class="pr-5 hover:underline"><a href='#'> Istorijat </a></li>
+            <li class="pr-5"><a href='#'>Galerija</a></li>
+            <li class="pr-5"><a  href='#'>Manastir</a></li>
+            <li><a href='#'>Kontakt</a></li>
         </ul>
+         <button className='bg-blue-400 w-10 h-6 hover:bg-green-200  font-black'>EN</button>
     </nav>
+   
+    
 </div>
   );
 }

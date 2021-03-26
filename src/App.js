60205011d14c34ec7665288d8c5b4eb75a214af6
Nikola <zelezekas@gmail.com>
2021-03-26 09:57:12 +0100

    Gallery started

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React, { Component } from 'react'
-import { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Error from './Error.js'
-import Aboutus from './about-us'
-import Contact from './contact.js'
-import Footer from './Footer'
-import Header from './header'
-import Products from './products'
-import Recipes from './recipes'
-import Teachings from './teachings'
-import Tests from './Tests.js'
-import SkrolovaniMenu from './SkrolovaniMenu.js'
-import Gallery from './Gallery';
+import React, { Component } from "react";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Error from "./Error.js";
+import Aboutus from "./about-us";
+import Contact from "./contact.js";
+import Footer from "./Footer";
+import Header from "./header";
+import Products from "./products";
+import Recipes from "./recipes";
+import Teachings from "./teachings";
+import Tests from "./Tests.js";
+import SkrolovaniMenu from "./SkrolovaniMenu.js";
+import Gallery from "./Gallery";
 
 const array = [
-  { component: Aboutus, string: 'aboutus', path: '/aboutus' },
-  { component: Products, string: 'products', path: '/products' },
-  { component: Recipes, string: 'recipes', path: '/recipes' },
-  { component: Teachings, string: 'teachings', path: '/teachings' },
-  { component: Contact, string: 'contact', path: '/contact' },
-  { component: Tests, string: 'tests', path: '/tests' },
-]
+  { component: Aboutus, string: "aboutus", path: "/aboutus" },
+  { component: Products, string: "products", path: "/products" },
+  { component: Recipes, string: "recipes", path: "/recipes" },
+  { component: Teachings, string: "teachings", path: "/teachings" },
+  { component: Contact, string: "contact", path: "/contact" },
+  { component: Tests, string: "tests", path: "/tests" },
+];
 
 const App = () => {
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     window.onscroll = () => {
-      window.pageYOffset > 100 ? setShow(true) : setShow(false)
-    }
-  })
+      window.pageYOffset > 100 ? setShow(true) : setShow(false);
+    };
+  });
 
   return (
-    <div className='container mx-auto   mr-4 ml-4'>
+    <div className="">
       <Router>
         <Header />
         {show && <SkrolovaniMenu />}
@@ -41,11 +41,11 @@ const App = () => {
             {<obj.component />}
           </Route>
         ))}
-        <Gallery/>
+        <Gallery />
         <Footer />
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="w-screen justify-between flex flex-col fixed bottom-0 p-5 bg-blue-200 md:flex-row">
+        <footer className="w-screen justify-between flex flex-col absolute bottom-0 p-5 bg-blue-200 md:flex-row">
             <a href="#" className="text-gray-600 hover:text-gray-100">Services</a>
             <a href="#" className="text-gray-600 hover:text-gray-100">Products</a>
             <a href="#" className="text-gray-600 hover:text-gray-100">Portfolio</a>

8c91d69f1cce715bbaaf51dfe39423900d51d433
Milutin <milutin.radonjic@limitforex.com>
2021-03-26 19:29:14 +0100

    Adds prettier formating settings, refactors containter sizes in tailwind config

--- src/App.js
+++ src/App.js
@@ -1,59 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './Home.js'
-import Aboutus from './about-us'
-import Contact from './contact'
-import Footer from './Footer'
-import Header from './header'
-import Products from './products'
-import Recipes from './recipes'
-import Scrollmenu from './Scrollmenu'
-import Teachings from './teachings'
-import Tests from './Tests'
-import PageNotFound from './PageNotFound'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home.js';
+import Aboutus from './about-us';
+import Contact from './contact';
+import Footer from './Footer';
+import Header from './header';
+import Products from './products';
+import Recipes from './recipes';
+import Scrollmenu from './Scrollmenu';
+import Teachings from './teachings';
+import Tests from './Tests';
+import PageNotFound from './PageNotFound';
 
 const arrayOfComponents = [
-  { component: Aboutus, string: 'aboutus', path: '/aboutus' },
+  { component: Aboutus, string: 'about-us', path: '/about-us' },
   { component: Products, string: 'products', path: '/products' },
   { component: Recipes, string: 'recipes', path: '/recipes' },
   { component: Teachings, string: 'teachings', path: '/teachings' },
   { component: Contact, string: 'contact', path: '/contact' },
   { component: Tests, string: 'tests', path: '/tests' },
-]
+];
 
 const App = () => {
-  const [pos, setPos] = useState(false)
+  const [pos, setPos] = useState(false);
 
   useEffect(() => {
     document.onscroll = (e) => {
-      let scrolled = document.scrollingElement.scrollTop
-      scrolled >= 96 ? setPos(true) : setPos(false)
-    }
-  }, [])
+      let scrolled = document.scrollingElement.scrollTop;
+      scrolled >= 96 ? setPos(true) : setPos(false);
+    };
+  }, []);
   return (
-    <div className='container mx-auto px-4'>
-      <Router>
-        <Header />
-        <Footer />
+    <Router>
+      <Header />
+      <Footer />
 
-        {pos && <Scrollmenu />}
-        <Switch>
-          <Route exact path='/'>
-            <Home />
+      {pos && <Scrollmenu />}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        {arrayOfComponents.map((component) => (
+          <Route key={component.string} path={component.path}>
+            {<component.component />}
           </Route>
-          {arrayOfComponents.map((component) => (
-            <Route key={component.string} path={component.path}>
-              {<component.component />}
-            </Route>
-          ))}
+        ))}
 
-          <Route path='*'>
-            <PageNotFound />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  )
-}
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/header.js
+++ src/header.js
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const firstMenu = [
   { sr: 'O nama', en: 'aboutus', slika: '../slike/slika1.png' },
   { sr: 'Recepti', en: 'recipes', slika: '../slike/slika2.png' },
   { sr: 'Pouke', en: 'teachings', slika: '../slike/slika3.png' },
-]
+];
 const secondMenu = [
   { sr: 'Kontakt', en: 'contact', slika: '../slike/slika4.png' },
   { sr: 'Proizvodi', en: 'products', slika: '../slike/slika5.png' },
   { sr: 'Test', en: 'tests', slika: '../slike/slika6.png' },
-]
+];
 
 const Header = (props) => {
-  const [isOpen, setIsOpen] = useState(true)
-  const [menu, setMenu] = useState([])
-  const [en, setEn] = useState([])
+  const [isOpen, setIsOpen] = useState(true);
+  const [menu, setMenu] = useState([]);
+  const [en, setEn] = useState([]);
 
   const language = () => {
-    en.length === 0 ? setEn(['EN', 'SR']) : setEn([])
-  }
+    en.length === 0 ? setEn(['EN', 'SR']) : setEn([]);
+  };
 
   useEffect(() => {
     isOpen
@@ -31,99 +31,87 @@ const Header = (props) => {
           'contact',
           'tests',
         ])
-      : setMenu([])
-  }, [isOpen])
+      : setMenu([]);
+  }, [isOpen]);
 
   return (
-    <div on className='container  mx-auto relative bg-blue-400  h-24'>
-      <nav className='flex md:ml-24 justify-between'>
-        <ul className='hidden md:flex    pr-3  '>
-          {firstMenu.map((item) => (
-            <NavLink
-              activeClassName='is-active'
-              className=' flex-none'
-              key={item.en}
-              to={item.en}
-            >
-              {' '}
-              <li className=' font-bold pr-2 pl-2 hover:underline hover:text-blue-500'>
-                {item.sr}
-              </li>
-              <img
-                alt=''
-                src={item.slika}
-                className='item-image ml-4 w-12 h-12 rounded-full'
-              ></img>
-            </NavLink>
-          ))}
-        </ul>
-        <NavLink activeClassName='is-active-logo' exact to='/'>
-          <img
-            src='../slike/slika7.png'
-            className='logo w-24 h-24'
-            alt=''
-          ></img>
-        </NavLink>
-
-        <ul className='hidden md:flex   pr-3  '>
-          {secondMenu.map((item) => (
-            <NavLink
-              activeClassName='is-active'
-              className='flex-none'
-              key={item.en}
-              to={item.en}
-            >
-              <li className=' font-bold pr-2 pl-2 hover:underline hover:text-blue-500'>
-                {item.sr}
-              </li>
-              <img
-                alt=''
-                src={item.slika}
-                className='item-image  ml-4 w-12 h-12 border rounded-3xl'
-              ></img>
-            </NavLink>
-          ))}
-        </ul>
-        <div className='mr-4'>
-          <img
-            alt=''
-            onClick={() => setIsOpen(!isOpen)}
-            className='w-12 h-12 md:hidden hover:bg-blue-300'
-            src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png'
-          ></img>
-
-          <div className=' ml-4'>
-            <button
-              onClick={language}
-              className=' rounded-sm  w-5 h-5 hover:text-white mb-1'
-            >
-              EN
-            </button>
-            <div className='flex flex-col   text-center  text-red-500'>
-              {en.map((lan) => (
-                <button key={lan} className='hover:text-black text-xs'>
-                  {lan}
-                </button>
+    <div className="bg-blue-400">
+      <div className="container mx-auto px-4">
+        <div className="relative h-24">
+          <nav className="flex md:ml-24 justify-between">
+            <ul className="hidden md:flex    pr-3  ">
+              {firstMenu.map((item) => (
+                <NavLink
+                  activeClassName="is-active"
+                  className=" flex-none"
+                  key={item.en}
+                  to={item.en}
+                >
+                  {' '}
+                  <li className=" font-bold pr-2 pl-2 hover:underline hover:text-blue-500">
+                    {item.sr}
+                  </li>
+                  <img
+                    alt=""
+                    src={item.slika}
+                    className="item-image ml-4 w-12 h-12 rounded-full"
+                  ></img>
+                </NavLink>
               ))}
-            </div>
-          </div>
-        </div>
-      </nav>
+            </ul>
+            <NavLink activeClassName="is-active-logo" exact to="/">
+              <img
+                src="../slike/slika7.png"
+                className="logo w-24 h-24"
+                alt=""
+              ></img>
+            </NavLink>
 
-      <ul className=' md:hidden divide-y divide-blue-300 text-center text-lg font-bold bg-gray-500 relative'>
-        {menu.map((item) => (
-          <Link onClick={() => setIsOpen(!isOpen)} to={item} key={item}>
-            <li className='h-10 p-2 '>{item}</li>
-          </Link>
-        ))}
-      </ul>
-      {/* <div className='w-full h-20 text-center bg-blue-200 text-white font-sans text-42 flex justify-center'>
-        <span className='flex sm:hidden'>XS</span>
-        <span className='hidden sm:flex md:hidden'>SM</span>
-        <span className='hidden md:flex lg:hidden'>MD</span>
-        <span className='hidden lg:flex '>LG</span>
-      </div> */}
+            <ul className="hidden md:flex   pr-3  ">
+              {secondMenu.map((item) => (
+                <NavLink
+                  activeClassName="is-active"
+                  className="flex-none"
+                  key={item.en}
+                  to={item.en}
+                >
+                  <li className=" font-bold pr-2 pl-2 hover:underline hover:text-blue-500">
+                    {item.sr}
+                  </li>
+                  <img
+                    alt=""
+                    src={item.slika}
+                    className="item-image  ml-4 w-12 h-12 border rounded-3xl"
+                  ></img>
+                </NavLink>
+              ))}
+            </ul>
+            <div className="mr-4">
+              <img
+                alt=""
+                onClick={() => setIsOpen(!isOpen)}
+                className="w-12 h-12 md:hidden hover:bg-blue-300"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"
+              ></img>
+            </div>
+          </nav>
+
+          <ul className=" md:hidden divide-y divide-blue-300 text-center text-lg font-bold bg-gray-500 relative">
+            {menu.map((item) => (
+              <Link onClick={() => setIsOpen(!isOpen)} to={item} key={item}>
+                <li className="h-10 p-2 ">{item}</li>
+              </Link>
+            ))}
+          </ul>
+          {/* <div className='w-full h-20 text-center bg-blue-200 text-white font-sans text-42 flex justify-center'>
+            <span className='flex sm:hidden'>XS</span>
+            <span className='hidden sm:flex md:hidden'>SM</span>
+            <span className='hidden md:flex lg:hidden'>MD</span>
+            <span className='hidden lg:flex '>LG</span>
+          </div> */}
+        </div>
+      </div>
     </div>
-  )
-}
-export default Header
+  );
+};
+export default Header;

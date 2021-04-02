2b3e51b7a986f34daaf4d611f332ed1c21d4c9f1
Nikola <zelezekas@gmail.com>
2021-04-02 17:29:58 +0200

    Gallery updated with modal and listing handlers

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,23 @@
 const Footer = () => {
-    return (
-        <footer className="w-screen justify-between flex flex-col absolute bottom-0 p-5 bg-blue-200 md:flex-row">
-            <a href="#" className="text-gray-600 hover:text-gray-100">Services</a>
-            <a href="#" className="text-gray-600 hover:text-gray-100">Products</a>
-            <a href="#" className="text-gray-600 hover:text-gray-100">Portfolio</a>
-            <a href="#" className="text-gray-600 hover:text-gray-100">About</a>
-            <p href="#" className="text-gray-100 italic">Powered by ClickNik©</p>
-        </footer>
-    )
-}
+	return (
+		<footer className="container mx-auto w-screen justify-between flex flex-col bottom-0 p-5 bg-blue-200 md:flex-row">
+			<a href="#" className="text-gray-600 hover:text-gray-100">
+				Services
+			</a>
+			<a href="#" className="text-gray-600 hover:text-gray-100">
+				Products
+			</a>
+			<a href="#" className="text-gray-600 hover:text-gray-100">
+				Portfolio
+			</a>
+			<a href="#" className="text-gray-600 hover:text-gray-100">
+				About
+			</a>
+			<p href="#" className="text-gray-100 italic">
+				Powered by ClickNik©
+			</p>
+		</footer>
+	);
+};
 
 export default Footer;
--- src/header.js
+++ src/header.js
@@ -1,101 +1,101 @@
-import { useEffect, useState } from 'react'
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const firstMenu = ['aboutus', 'recipes', 'teachings']
-const secondMenu = ['contact', 'products', 'tests']
+const firstMenu = ['aboutus', 'recipes', 'teachings'];
+const secondMenu = ['contact', 'products', 'tests'];
 
 const Header = (props) => {
-  const [isOpen, setIsOpen] = useState(true)
-  const [menu, setMenu] = useState([])
-  const [en, setEn] = useState([])
+	const [isOpen, setIsOpen] = useState(false);
+	const [menu, setMenu] = useState([]);
+	const [en, setEn] = useState([]);
 
-  const language = () => {
-    en.length === 0 ? setEn(['EN', 'SR']) : setEn([])
-  }
+	const language = () => {
+		en.length === 0 ? setEn(['EN', 'SR']) : setEn([]);
+	};
 
-  useEffect(() => {
-    isOpen
-      ? setMenu([
-          'aboutus',
-          'products',
-          'recipes',
-          'teachings',
-          'contact',
-          'tests',
-        ])
-      : setMenu([])
-  }, [isOpen])
+	useEffect(() => {
+		isOpen
+			? setMenu([
+					'aboutus',
+					'products',
+					'recipes',
+					'teachings',
+					'contact',
+					'tests',
+			  ])
+			: setMenu([]);
+	}, [isOpen]);
 
-  return (
-    <div className='container  mx-auto relative bg-blue-400  h-24'>
-      <nav className='flex md:ml-24 justify-between'>
-        <ul className='hidden md:flex    pr-3  '>
-          {firstMenu.map((item) => (
-            <Link className='flex-none' key={item} to={item}>
-              {' '}
-              <li className=' font-bold pr-2 pl-2 hover:underline hover:text-blue-500'>
-                {item}
-              </li>
-              <div className='ml-4 w-12 h-12 border rounded-3xl '>vinjeta</div>
-            </Link>
-          ))}
-        </ul>
-        <Link to='/'>
-          <div className=' w-20 h-20 border rounded-3xl text-center p-4'>
-            LOGO
-          </div>
-        </Link>
+	return (
+		<div className="container  mx-auto relative bg-blue-400  h-24">
+			<nav className="flex md:ml-24 justify-between">
+				<ul className="hidden md:flex    pr-3  ">
+					{firstMenu.map((item) => (
+						<Link className="flex-none" key={item} to={item}>
+							{' '}
+							<li className=" font-bold pr-2 pl-2 hover:underline hover:text-blue-500">
+								{item}
+							</li>
+							<div className="ml-4 w-12 h-12 border rounded-3xl ">vinjeta</div>
+						</Link>
+					))}
+				</ul>
+				<Link to="/">
+					<div className=" w-20 h-20 border rounded-3xl text-center p-4">
+						LOGO
+					</div>
+				</Link>
 
-        <ul className='hidden md:flex   pr-3  '>
-          {secondMenu.map((item) => (
-            <Link className='flex-none' key={item} to={item}>
-              <li className=' font-bold pr-2 pl-2 hover:underline hover:text-blue-500'>
-                {item}
-              </li>
-              <div className='ml-4 w-12 h-12 border rounded-3xl '>vinjeta</div>
-            </Link>
-          ))}
-        </ul>
-        <div className='mr-4'>
-          <img
-            onClick={() => setIsOpen(!isOpen)}
-            className='   w-12 h-12 md:hidden hover:bg-blue-300'
-            src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png'
-          ></img>
+				<ul className="hidden md:flex   pr-3  ">
+					{secondMenu.map((item) => (
+						<Link className="flex-none" key={item} to={item}>
+							<li className=" font-bold pr-2 pl-2 hover:underline hover:text-blue-500">
+								{item}
+							</li>
+							<div className="ml-4 w-12 h-12 border rounded-3xl ">vinjeta</div>
+						</Link>
+					))}
+				</ul>
+				<div className="mr-4">
+					<img
+						onClick={() => setIsOpen(!isOpen)}
+						className="   w-12 h-12 md:hidden hover:bg-blue-300"
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"
+					></img>
 
-          <div className=' ml-4'>
-            <button
-              onClick={language}
-              className=' rounded-sm  w-5 h-5 hover:text-white mb-1'
-            >
-              EN
-            </button>
-            <div className='flex flex-col   text-center  text-red-500'>
-              {en.map((lan) => (
-                <button key={lan} className='hover:text-black text-xs'>
-                  {lan}
-                </button>
-              ))}
-            </div>
-          </div>
-        </div>
-      </nav>
+					<div className=" ml-4">
+						<button
+							onClick={language}
+							className=" rounded-sm  w-5 h-5 hover:text-white mb-1"
+						>
+							EN
+						</button>
+						<div className="flex flex-col   text-center  text-red-500">
+							{en.map((lan) => (
+								<button key={lan} className="hover:text-black text-xs">
+									{lan}
+								</button>
+							))}
+						</div>
+					</div>
+				</div>
+			</nav>
 
-      <ul className=' md:hidden divide-y divide-blue-300 text-center text-lg font-bold bg-gray-500 relative'>
-        {menu.map((item) => (
-          <Link onClick={() => setIsOpen(!isOpen)} to={item} key={item}>
-            <li className='h-10 p-2 '>{item}</li>
-          </Link>
-        ))}
-      </ul>
-      <div className='w-full h-20 text-center bg-blue-200 text-white font-sans text-42 flex justify-center'>
-        <span className='flex sm:hidden'>XS</span>
-        <span className='hidden sm:flex md:hidden'>SM</span>
-        <span className='hidden md:flex lg:hidden'>MD</span>
-        <span className='hidden lg:flex '>LG</span>
-      </div>
-    </div>
-  )
-}
-export default Header
+			<ul className=" md:hidden divide-y divide-blue-300 text-center text-lg font-bold bg-gray-500 relative">
+				{menu.map((item) => (
+					<Link onClick={() => setIsOpen(!isOpen)} to={item} key={item}>
+						<li className="h-10 p-2 ">{item}</li>
+					</Link>
+				))}
+			</ul>
+			<div className="w-full h-20 text-center bg-blue-200 text-white font-sans text-42 flex justify-center">
+				<span className="flex sm:hidden">XS</span>
+				<span className="hidden sm:flex md:hidden">SM</span>
+				<span className="hidden md:flex lg:hidden">MD</span>
+				<span className="hidden lg:flex ">LG</span>
+			</div>
+		</div>
+	);
+};
+export default Header;

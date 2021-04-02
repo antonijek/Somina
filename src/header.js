c1a3bcbc62165660eaf3f08ccf3a20d5f9406d21
antonijek <antonijek@yahoo.com>
2021-04-02 19:03:43 +0200

    w-full correction

--- src/header.js
+++ src/header.js
@@ -37,10 +37,10 @@ const Header = (props) => {
 
   return (
     <div className="bg-blue-400 ">
-      <div className="mx-auto ">
-        <div className="relative h-28">
-          <nav className="flex justify-between items-center ">
-            <ul className="hidden md:flex pr-3 ml-8  p-4">
+      <div className="relative h-32">
+        <div className="container mx-auto p-4">
+          <nav className="flex justify-between items-center">
+            <ul className="hidden md:flex pr-3  ">
               {firstMenu.map((item) => (
                 <NavLink
                   activeClassName="is-active"
@@ -63,7 +63,7 @@ const Header = (props) => {
             <NavLink activeClassName="is-active-logo" exact to="">
               <img
                 src="../slike/slika7.png"
-                className="logo w-24 h-24 my-2 mx-2"
+                className="logo w-24 h-24"
                 alt=""
               ></img>
             </NavLink>
@@ -92,31 +92,30 @@ const Header = (props) => {
                 onClick={() => {
                   setIsOpen(!isOpen);
                 }}
-                className="w-16 mx-8"
+                className="w-16"
                 src={icon}
                 alt=""
               />
             </div>
           </nav>
-
-          <ul className=" md:hidden text-center text-lg font-bold bg-gray-500 relative z-100">
-            {menu.map((item) => (
-              <Link
-                onClick={() => setIsOpen(!isOpen)}
-                to={item.path}
-                key={item.path}
-              >
-                <li className="h-10 p-2 hover:text-red-500">{item.name}</li>
-              </Link>
-            ))}
-          </ul>
-          {/* <div className='w-full h-20 text-center bg-blue-200 text-white font-sans text-42 flex justify-center'>
+        </div>
+        <ul className="md:hidden  text-center text-lg font-bold bg-gray-500 relative z-100">
+          {menu.map((item) => (
+            <Link
+              onClick={() => setIsOpen(!isOpen)}
+              to={item.path}
+              key={item.path}
+            >
+              <li className="h-10 p-2 hover:text-red-500">{item.name}</li>
+            </Link>
+          ))}
+        </ul>
+        {/* <div className='w-full h-20 text-center bg-blue-200 text-white font-sans text-42 flex justify-center'>
             <span className='flex sm:hidden'>XS</span>
             <span className='hidden sm:flex md:hidden'>SM</span>
             <span className='hidden md:flex lg:hidden'>MD</span>
             <span className='hidden lg:flex '>LG</span>
           </div> */}
-        </div>
       </div>
     </div>
   );

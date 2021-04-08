7dd35c4bbca52b2562f0dc939ca7c406ff1a7a24
antonijek <antonijek@yahoo.com>
2021-04-08 16:49:39 +0200

    manifest json

--- src/products.js
+++ src/products.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+} from 'react-router-dom';
 
 const Proiz = [
   {
@@ -7,36 +15,42 @@ const Proiz = [
     id: 1,
     cijena: '10 evra',
     slika: 'ovdje ce da ide neka slika',
+    path: '/melem',
   },
   {
     name: 'med',
     id: 2,
     cijena: '11 evra',
     slika: 'ovdje ce da ide neka slika',
+    path: '/med',
   },
   {
     name: 'kapi',
     id: 3,
     cijena: '102 evra',
     slika: 'ovdje ce da ide neka slika',
+    path: '/kapi',
   },
   {
     name: 'rakija',
     id: 4,
     cijena: '13 evra',
     slika: 'ovdje ce da ide neka slika',
+    path: '/rakija',
   },
   {
     name: 'caj',
     id: 5,
     cijena: '14 evra',
     slika: 'ovdje ce da ide neka slika',
+    path: '/caj',
   },
   {
     name: 'sir',
     id: 6,
     cijena: '15 evra',
     slika: 'ovdje ce da ide neka slika',
+    path: '/sir',
   },
 ];
 
@@ -46,22 +60,25 @@ const Proizvod = (props) => {
     setPro(e);
     console.log(e);
   };
-
+  let { path, url } = useRouteMatch();
   return (
     <div className=" container  mx-auto relative">
       {Proiz.map((item) => (
-        <div key={item.id}>
-          <h2
-            className=" bg-gray-300 text-center"
-            onClick={(e) => {
-              dugme(item);
-            }}
-          >
-            {item.name}
-          </h2>
+        <div key={item.id} className=" bg-gray-300 text-center">
+          <Link to={url + item.path}>{item.name}</Link>
         </div>
       ))}
 
+      <Switch>
+        {Proiz.map((item) => (
+          <Route path={path + item.path}>
+            <h2>{item.name}</h2>
+            <p>{item.cijena}</p>
+            <p>{item.id}</p>
+            <p>{item.slika}</p>
+          </Route>
+        ))}
+      </Switch>
       <div className="mt-8 text-center">
         <p className="bg-gray-500 text-green-500 text-2xl">{pro.name}</p>
         <p className="bg-gray-500  text-green-500 text-2xl">{pro.id}</p>
